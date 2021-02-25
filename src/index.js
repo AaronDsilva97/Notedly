6ff40d45e668ff82008ff07cceb99fa358eb7180
AaronDsilva97 <aaronxdsilva@gmail.com>
2021-02-25 22:06:00 +0530

    CRUD

--- src/index.js
+++ src/index.js
@@ -1,58 +1,28 @@
 const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 require("dotenv").config();
 const db = require("./db");
 const models = require("./models");
-
+const resolvers = require("./resolvers");
 const app = express();
+
+//GraphQl Scheme
+const typeDefs = require("./schema");
 
 //dynamic port
 const port = process.env.PORT || 3000;
 
 const DB_HOST = process.env.DB_HOST;
 
-//GraphQl Scheme
-const typeDefs = gql`
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-
-  type Query {
-    hello: String
-    notes: [Note!]!
-    note(id: ID!): Note!
-  }
-
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    },
-  },
-
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: "Aaron Dsilva",
-      });
-    },
-  },
-};
-
 db.connect(DB_HOST);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  },
+});
 
 server.applyMiddleware({ app, path: "/api" });
 

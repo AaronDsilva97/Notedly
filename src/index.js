4cfb80de09a8cd1963fdb7d03ca98591be246182
AaronDsilva97 <aaronxdsilva@gmail.com>
2021-02-25 23:12:37 +0530

    User Authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const db = require("./db");
 const models = require("./models");
 const resolvers = require("./resolvers");
 const app = express();
+
+// get the user info from a JWT
+const getUser = (token) => {
+  if (token) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      // if there's a problem with the token, throw an error
+      throw new Error("Session invalid");
+    }
+  }
+};
 
 //GraphQl Scheme
 const typeDefs = require("./schema");
@@ -19,8 +33,15 @@ db.connect(DB_HOST);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    return { models };
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // for now, let's log the user to the console:
+    console.log(user);
+    // add the db models and the user to the context
+    return { models, user };
   },
 });
 
--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,10 +1,22 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {
+  AuthenticationError,
+  ForbiddenError,
+} = require("apollo-server-express");
+require("dotenv").config();
+const gravatar = require("../util/gravatar");
+
 module.exports = {
+  //Addind a new note
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
       content: args.content,
       author: "Aaron Dsilva",
     });
   },
+
+  //Deleting a note
   deleteNote: async (parent, { id }, { models }) => {
     try {
       await models.Note.findOneAndRemove({ _id: id });
@@ -13,6 +25,8 @@ module.exports = {
       return false;
     }
   },
+
+  //Updating a note
   updateNote: async (parent, { content, id }, { models }) => {
     return await models.Note.findOneAndUpdate(
       {
@@ -27,5 +41,47 @@ module.exports = {
         new: true,
       }
     );
+  },
+
+  //Sign up
+  signUp: async (parent, { username, email, password }, { models }) => {
+    email = email.trim().toLowerCase();
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const avatar = gravatar(email);
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed,
+      });
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+      throw new Error("Error creating account");
+    }
+  },
+
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (!user) {
+      throw new AuthenticationError("Error Signing in");
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError("Password incorrect");
+    }
+
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   },
 };

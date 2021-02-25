71f718e42b4bac7dcc23d91fa7fdf69e16911fb4
AaronDsilva97 <aaronxdsilva@gmail.com>
2021-02-25 08:48:16 +0530

    Basic Graphql Server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
 const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
 
 const app = express();
+
+let notes = [
+  { id: "1", content: "This is a note", author: "Adam Scott" },
+  { id: "2", content: "This is another note", author: "Harlow Everly" },
+  { id: "3", content: "Oh hey look, another note!", author: "Riley Harrison" },
+];
+
+//GraphQl Scheme
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find((note) => note.id === args.id);
+    },
+  },
+
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: "Adam Scott",
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: "/api" });
 
 //dynamic port
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
 
-app.listen(port, () => console.log("Server has begun on port 3000"));
+app.listen(port, () =>
+  console.log(
+    `Server has begun on port http:localhost:${port}${server.graphqlPath}`
+  )
+);

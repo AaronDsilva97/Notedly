d92e8b7282038de34b6541faa0706a1086563e90
AaronDsilva97 <aaronxdsilva@gmail.com>
2021-02-27 09:40:48 +0530

    Stopped tracking env.local, and env.staging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
 const jwt = require("jsonwebtoken");
+const depthLimit = require("graphql-depth-limit");
+const { createComplexityLimitRule } = require("graphql-validation-complexity");
 require("dotenv").config();
 const db = require("./db");
 const models = require("./models");
 const resolvers = require("./resolvers");
+
 const app = express();
+
+app.use(helmet());
+
+app.use(cors());
 
 // get the user info from a JWT
 const getUser = (token) => {
@@ -33,13 +42,14 @@ db.connect(DB_HOST);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
+  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+  context: async ({ req }) => {
     // get the user token from the headers
     const token = req.headers.authorization;
     // try to retrieve a user with the token
-    const user = getUser(token);
+    const user = await getUser(token);
     // for now, let's log the user to the console:
-    console.log(user);
+    // console.log(user);
     // add the db models and the user to the context
     return { models, user };
   },
